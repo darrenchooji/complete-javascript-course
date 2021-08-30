f011f91d57b784d2e9db48e3346c986c124a4318
darrenchooji <darrenchoojuninn@gmail.com>
2021-08-30 00:28:39 -0700

    Video 10: Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,16 @@
 let js = 'amazing';
-if (js === 'amazing') alert("JavaScript is Fun!");
+
+console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+
+// CamelCase naming convention
+let firstName = "Darren";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstSchool = "Pei Chun Public School";
+let myCurrentSchool = "National University of Singapore";
+console.log(myFirstSchool);
+console.log(myCurrentSchool);
