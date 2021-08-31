9d9792daf8db520491808e87378447d45182be84
darrenchooji <darrenchoojuninn@gmail.com>
2021-08-31 01:00:39 -0700

    Video 10 and 11

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -5,12 +5,33 @@ console.log("Jonas");
 console.log(23);
 
 // CamelCase naming convention
-let firstName = "Darren";
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-
 let myFirstSchool = "Pei Chun Public School";
 let myCurrentSchool = "National University of Singapore";
 console.log(myFirstSchool);
 console.log(myCurrentSchool);
+
+/*
+From ES2020, there is a new primitive data type. BigInt (for larger integers that the Number data type cannot hold)
+JavaScript has dynamic typing (automatically determines the data type of the value)
+Assign new value of a different data type to the same variable.
+*/
+
+let isJavaScriptFun = true;
+console.log(isJavaScriptFun);
+
+// Example of dynamic typing
+let myRelationshipStatus;
+console.log(typeof myRelationshipStatus); // undefined
+myRelationshipStatus = "single";
+console.log(typeof myRelationshipStatus); // string
+myRelationshipStatus = 1;
+console.log(typeof myRelationshipStatus); // number
+
+/*
+ Difference between var, let, and const
+const does not the value of the variable to be changed
+var is function-scope
+let is block-scope
+*/
+const myLastName = "Choo";
+console.log(typeof myLastName);
